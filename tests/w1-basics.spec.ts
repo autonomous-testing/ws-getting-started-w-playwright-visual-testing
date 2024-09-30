e29e35085de4bd94a85277a84f041c1fa6a32b80
Marcel Veselka <marcel.veselka@tesena.com>
2024-09-30 11:49:11 +0200

    feat: simplified first basic example

--- tests/w1-basics.spec.ts
+++ tests/w1-basics.spec.ts
@@ -1,20 +1,18 @@
 import { Wopee } from "@wopee-io/wopee.pw";
 import { test } from "@playwright/test";
 
-let wopee: Wopee;
+let wopee = new Wopee();
 const baseUrl = "https://dronjo.wopee.io";
 
 test.describe("Example w. Wopee.io", () => {
-  test.beforeAll(async () => {
-    wopee = new Wopee();
-    await wopee.startSuite(`First demo test suite`);
-  });
-
   test("Home", async ({ page }, testInfo) => {
-    await wopee.startScenario(testInfo.title);
-
     await page.goto(baseUrl);
-    await wopee.trackFullPage({ page, stepName: "Home page" });
+    await wopee.trackFullPage({
+      page,
+      stepName: "Home page",
+      scenarioName: testInfo.title,
+      suiteName: testInfo.project.name,
+    });
 
     await wopee.stopScenario();
   });
